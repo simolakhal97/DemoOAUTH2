6570a17b5754ea29a384262e7e18704df2ba0b1f
mohammed lakhal <mohammedlakha@gmail.coml>
2023-11-11 20:29:54 +0100

    first add

---
+++ src/main/java/com/example/OAUTH2/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.OAUTH2.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoController {
+    @GetMapping
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("hello oauth2");
+    }
+}
